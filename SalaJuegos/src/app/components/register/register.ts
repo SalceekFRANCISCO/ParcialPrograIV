6b7144ab20c54cf0dc59d4a8cd35a341c364d8b7
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2026-05-18 23:55:32 -0300

    mejorado el html y el css

--- SalaJuegos/src/app/components/register/register.ts
+++ SalaJuegos/src/app/components/register/register.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+// import { AuthService } from '../../services/auth';
+import { FormsModule } from "@angular/forms";
+// import { SupabaseService } from '../../services/supabase';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
