d2e77f61088611664d9abf21f28e74fe24cb1315
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2026-05-19 17:16:43 -0300

    espero funcione vercel con esto

---
+++ SalaJuegos/src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://lydrpzbceuzsatlpuufc.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5ZHJwemJjZXV6c2F0bHB1dWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1ODc0MDEsImV4cCI6MjA5NDE2MzQwMX0.MAEAouOXEYSz0rObzDMd4DzRNXTMQi8RJgF9lv1P1Go',
+}
