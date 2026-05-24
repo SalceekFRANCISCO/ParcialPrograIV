fbeac17f798e3cae19422884290d1ee5f71c4df3
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2026-05-24 19:34:17 -0300

    cambios en hanged

--- SalaJuegos/src/app/components/games/hanged/hanged.ts
+++ SalaJuegos/src/app/components/games/hanged/hanged.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HangedServices } from '../../../services/hanged';
 import { RouterLink } from "@angular/router";
 import { AuthService } from '../../../services/auth';
@@ -10,46 +10,70 @@ import { AuthService } from '../../../services/auth';
   styleUrl: './hanged.css',
 })
 export class Hanged {
+
   private hanged = inject(HangedServices);
+
   private auth = inject(AuthService);
 
   lives: number = 6;
-  secretWord = 'ANGULAR';
-  secretWordArray: string[] = []; // [_ _ _ _ _ _]
-  indexArray: number[] = [];
-  ArrayLetter: string[] = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-  arrayWord: string[] = this.secretWord.split(''); 
+  secretWord = 'ANGULAR';
+
+  secretWordArray: string[] = [];
+
+  indexArray: number[] = [];
+
+  ArrayLetter: string[] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z'
+  ];
+
+  arrayWord: string[] = this.secretWord.split('');
+
   usedLetters = new Set<string>();
-  
-  clock: string = '';
 
   gameOver = false;
-  
+
+  clock: string = '0s';
+
+  seconds: number = 0;
+
+  interval: any;
+
   constructor(){
+
     this.drawSpaces();
 
-    setInterval(() => {
-      
-      const date: Date = new Date();
-      
-      this.clock = date.toLocaleTimeString('es-AR',{
-        minute: '2-digit',
-        second: '2-digit'
-      });
-  
+    this.startTimer();
+  }
+
+  startTimer(){
+
+    this.interval = setInterval(() => {
+
+      if(!this.gameOver){
+
+        this.seconds++;
+
+        this.clock = `${this.seconds}s`;
+      }
+
     }, 1000);
   }
 
   private drawSpaces(){
-    for(let i: number = 0; i < this.secretWord.length; i++){
+
+    for(let i = 0; i < this.secretWord.length; i++){
+
       this.secretWordArray.push('_');
     }
-    
   }
 
-
   resetGame(){
+
+    clearInterval(this.interval);
 
     this.lives = 6;
 
@@ -59,96 +83,128 @@ export class Hanged {
 
     this.indexArray = [];
 
+    this.arrayWord = this.secretWord.split('');
+
     this.usedLetters.clear();
 
-    this.gameOver = (false);
-    
-    this.clock = '';
+    this.gameOver = false;
+
+    this.seconds = 0;
+
+    this.clock = '0s';
 
     this.drawSpaces();
-}
+
+    this.startTimer();
+  }
 
   sendValue(buttonWord: string){
-    
+
+    if(this.gameOver){
+
+      return;
+    }
+
     if(this.usedLetters.has(buttonWord)){
+
       return;
     }
 
     this.usedLetters.add(buttonWord);
 
-    if(this.arrayWord.includes(buttonWord)){ // [A N G U L A R] INCLUYE 'A'?
+    if(this.arrayWord.includes(buttonWord)){
 
       this.findArrayWord(buttonWord);
 
       this.checkVictory();
-    }
-    else{
-      this.lives--;
-      this.checkVictory();
-    
-    }
-  
 
+    } else {
+
+      this.lives--;
+
+      this.checkVictory();
+    }
   }
 
   checkLetter(buttonWord: string): boolean {
 
-    if(this.usedLetters.has(buttonWord)){
-      return false;
-    }
-
-    this.usedLetters.add(buttonWord);
-    return true;
+    return !this.usedLetters.has(buttonWord);
   }
 
   checkVictory(){
+
     if(!this.secretWordArray.includes('_')){
+
       console.log('GANASTE');
+
       this.gameOver = true;
+
+      clearInterval(this.interval);
+
+      this.saveGame(true);
     }
-    
+
     if(this.lives <= 0){
+
       console.log('PERDISTE');
+
       this.gameOver = true;
+
+      clearInterval(this.interval);
+
+      this.saveGame(false);
     }
-    
-    // this.hanged.sendData()
-    // enviar datos a database...
-    const id = 0;
-    const email = '';
-    const time = 0; 
-    const user = this.auth.currentUser()
-    user?.id
-    user?.email
-    if(user?.email){
-      this.hanged.sendData(id, user?.email, this.secretWord,this.gameOver,this.lives,this.usedLetters.size,time);
+  }
+
+  async saveGame(victory: boolean){
+
+    const user = this.auth.currentUser();
+
+    if(!user?.email){
+
+      return;
     }
 
+    await this.hanged.sendData(
 
+      user.email,
 
+      this.secretWord,
+
+      victory,
+
+      this.lives,
+
+      this.usedLetters.size,
+
+      this.seconds
+    );
   }
 
   findArrayWord(buttonWord: string){
-    if(this.arrayWord.includes(buttonWord)){ // [A N G U L A R] INCLUYE 'A'?
 
-      for(let i = 0; i < this.secretWord.length; i++){ //aca estamos buscando si hay mas de una coincidencia
+    if(this.arrayWord.includes(buttonWord)){
+
+      for(let i = 0; i < this.secretWord.length; i++){
+
         if(this.arrayWord[i] === buttonWord){
-          this.indexArray.push(i) // guardamos 2 valores, porque A aparece 2 veces
+
+          this.indexArray.push(i);
         }
       }
 
-      // console.log(this.indexArray); // indexArray = [0, 5]
-        
-        for(let i=0; i < this.secretWordArray.length; i++){ //recorremos el largo del arraysecreto
-          for(let j=0; j < this.indexArray.length; j++){ // recorremos el largo del array de indices
-            if (i === this.indexArray[j]){ // preguntamos si la posicion "i" es igual a algun elemento del indexArray. ej: 0 === 0 [0,5] 
-            this.secretWordArray[i] = buttonWord; // al array secreto en posicion de i, le metemos la palabra, ej: en secretWord[0] reemplazamos y ponemos la letra 'A'
-            }
+      for(let i = 0; i < this.secretWordArray.length; i++){
+
+        for(let j = 0; j < this.indexArray.length; j++){
+
+          if(i === this.indexArray[j]){
+
+            this.secretWordArray[i] = buttonWord;
+          }
         }
       }
+
       this.indexArray = [];
     }
-
   }
-
 }
--- SalaJuegos/src/app/services/hanged.ts
+++ SalaJuegos/src/app/services/hanged.ts
@@ -1,28 +1,46 @@
-import { inject, Injectable } from '@angular/core';
-import { SupabaseClient } from '@supabase/supabase-js/dist/index.cjs';
+import { Injectable } from '@angular/core';
+
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HangedServices {
-  private supabase = inject(SupabaseClient);
 
-  async sendData(id: number, user_email:string, word: string, victory: boolean, lives: number, selected_letters: number, game_time: number){
-    return await this.supabase.from('hanged').insert(
-      {
-        id: id, 
-        user_email: user_email,
-        word: word,
-        victory: victory,
-        lives: lives,
-        selected_letters: selected_letters,
-        game_time: game_time
-      }).then(({error}) => {
-            if(error) console.log("error: " +error.message);
+  private supabase: SupabaseClient = createClient(
+    environment.supabaseUrl,
+    environment.supabaseKey
+  );
+
+  constructor(){}
+
+  async sendData(
+    user_email: string,
+    word: string,
+    victory: boolean,
+    lives: number,
+    selected_letters: number,
+    game_time: number
+  ){
+
+    return await this.supabase
+      .from('hanged')
+      .insert({
+        user_email,
+        word,
+        victory,
+        lives,
+        selected_letters,
+        game_time
+      })
+      .then(({ error }) => {
+
+        if(error){
+
+          console.log('error: ' + error.message);
         }
-    )}
-
-
-
-
+      });
+  }
 }
