40b61bf1375ce8083ddb7a9a994c54f6e3c2e05f
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2026-05-24 22:23:19 -0300

    implementando el sprint4

---
+++ SalaJuegos/src/app/components/games/quiz/quiz.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  imports: [],
+  templateUrl: './quiz.html',
+  styleUrl: './quiz.css',
+})
+export class Quiz {
+
+// ○ Debe obtener los datos de una api.
+// ○ Puede ser una api de preguntas o una api con información a la que luego se le agregue la
+// funcionalidad de preguntados. Puede estar en inglés.
+// ○ Las opciones de elección deben ser botones.
+// ○ Al finalizar la partida, guardar en la base de datos: el usuario que jugó, cantidad de
+// preguntas acertadas, etc.
+
+}
