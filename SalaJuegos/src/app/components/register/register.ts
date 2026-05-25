a0d086a132fdbd4129514f397a275dbf9683e0d8
Francisco Salceek <salceekmunozfrancisco@gmail.com>
2026-05-25 17:12:19 -0300

    actualizado el logo

--- SalaJuegos/src/app/components/register/register.ts
+++ SalaJuegos/src/app/components/register/register.ts
@@ -1,7 +1,6 @@
 import { Component, computed, inject } from '@angular/core';
 import { AuthService } from '../../services/auth';
-import { FormsModule } from "@angular/forms";
-import { SupabaseService } from '../../services/supabase';
+import { FormsModule, NgForm } from "@angular/forms"; // Importamos NgForm
 
 @Component({
   selector: 'app-register',
@@ -19,34 +18,34 @@ export class Register {
   email: string = '';
   password: string = '';
 
-  async onSubmit(){
-    this.auth.registerUser(this.name,this.lastName,this.age,this.email,this.password);
-    
+
+  // Pasamos el formulario como parámetro para una doble validación segura
+  async onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return; // Si el frontend detecta un error, frena acá y no llama al servicio
+    }
+
+    this.auth.registerUser(this.name, this.lastName, this.age, this.email, this.password);
   }
 
   showError = computed(() => {
     const error = this.auth.errorMessage();
 
-    if(!error) return '';
+    if (!error) return '';
 
-    if(error.code === 'user_already_exists'){
+    if (error.code === 'user_already_exists') {
       return 'El usuario ya se encuentra registrado';
     }
 
-    if(error.code === 'weak_password'){
+    if (error.code === 'weak_password') {
       return 'La Contraseña debe tener un minimo de 6 caracteres';
     }
 
-    if(error.code === 'anonymous_provider_disabled' || error.code === 'validation_failed'){
+    if (error.code === 'anonymous_provider_disabled' || error.code === 'validation_failed') {
       return 'Todos los campos son obligatorios';
     }
 
-    else{
-      return '';
-    }
-    
-  })
-
-
-
+    return '';
+  });
+  
 }
